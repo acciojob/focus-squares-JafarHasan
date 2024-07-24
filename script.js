8cd3e65177257dd5f709739ac656360ab2597ba0
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-24 13:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const squares=document.querySelectorAll('.square');
+squares.addEventListener('mouseover',handleMouseOver);
+squares.addEventListener('mouseout',handleMouseOut);
+
+function handleMouseOver() {
+	squares.forEach((square)=>{
+		if(square!=event.target){
+			square.style.backgroundColor="#6F4E37";
+		}
+	});
+	
+}
+
+function handleMouseOut() {
+	
+	
+}
