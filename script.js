c2fc8ab09e7815d2cdda400b55b1f4c5eccbe8fe
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-24 17:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-//your JS code here. If required.
-const squares=document.querySelectorAll('.square');
+const squares = document.querySelectorAll('.square');
 
-squares.forEach((square)=>{
-	square.addEventListener('mouseover',handleMouseOver);
-	square.addEventListener('mouseout',handleMouseOut);
-})
-
+squares.forEach((square) => {
+    square.addEventListener('mouseover', handleMouseOver);
+    square.addEventListener('mouseout', handleMouseOut);
+});
 
 function handleMouseOver(event) {
-	squares.forEach((square)=>{
-		if(square!=event.target){
-			square.style.backgroundColor="#6F4E37"; /*coffee color*/
-		}
-	});
-	
+    squares.forEach((square) => {
+        if (square != event.target) {
+            square.style.backgroundColor = "#6F4E37"; /* coffee color */
+        }
+    });
 }
 
 function handleMouseOut(event) {
-	squares.forEach((square)=>{
-			square.style.backgroundColor="#E6E6FA"; /*levender color*/
-	})
-	
+    squares.forEach((square) => {
+        square.style.backgroundColor = "#E6E6FA"; /* lavender color */
+    });
 }
